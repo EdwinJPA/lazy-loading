13e67bc29cb3bb6505a0cf07605070ddbb525110
J6-Babbage <edwinjpr11@gmail.com>
2022-11-24 16:54:40 -0500

    finally project implementing canActivated

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -6,9 +6,10 @@ import {
   UrlSegment,
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
-  UrlTree,
+  Router,
 } from '@angular/router';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { Auth } from '../interfaces/auth.interface';
 import { AuthService } from '../services/auth.service';
 
@@ -20,16 +21,39 @@ export class AuthGuard implements CanLoad, CanActivate {
     return this.authService.auth;
   }
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ): boolean {
-    return true;
+  ): Observable<boolean> {
+    return this.authService.verificaAutenticacion().pipe(
+      tap((estaAutenticado) => {
+        if (!estaAutenticado) {
+          this.router.navigate(['./auth/login']);
+        }
+      })
+    );
+    //   if (this.auth.id) {
+    //     return true;
+    //   } else {
+    //     console.log('Bloqueado por el AuthGuard - canActivate');
+    //     return false;
+    //   }
   }
 
-  canLoad(route: Route, segments: UrlSegment[]): boolean {
-    console.log('Bloquedo por el AuthGuard');
-    return this.auth.id ? true : false;
+  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> {
+    return this.authService.verificaAutenticacion().pipe(
+      tap((estaAutenticado) => {
+        if (!estaAutenticado) {
+          this.router.navigate(['./auth/login']);
+        }
+      })
+    );
+    //   if (this.auth.id ) {
+    //     return true;
+    //   } else {
+    //     console.log('Bloqueado por el AuthGuard - canLoad');
+    //     return false;
+    //   }
   }
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Auth } from '../interfaces/auth.interface';
 
@@ -18,10 +18,24 @@ export class AuthService {
 
   constructor(private http: HttpClient) {}
 
+  verificaAutenticacion(): Observable<boolean> {
+    if (!localStorage.getItem('token')) {
+      return of(false);
+    }
+
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`).pipe(
+      map((auth) => {
+        this._auth = auth;
+        return true;
+      })
+    );
+  }
+
   login(): Observable<Auth> {
-    return this.http
-      .get<Auth>(`${this.baseUrl}/usuarios/1`)
-      .pipe(tap((auth) => (this._auth = auth)));
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`).pipe(
+      tap((auth) => (this._auth = auth)),
+      tap((auth) => localStorage.setItem('token', auth.id))
+    );
   }
 
   logout() {
